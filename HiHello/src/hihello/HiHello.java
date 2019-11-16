f3846d347573dedc3075d6e5912a4ac727674c85
Lorenzo-Battistutta-Peano-4A <56471095+Lorenzo-Battistutta-Peano-4A@users.noreply.github.com>
2019-11-16 09:19:31 +0100

    Aggiunto progetto "HiHello"

---
+++ HiHello/src/hihello/HiHello.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hihello;
+
+/**
+ *
+ * @author perilli.alberto
+ */
+public class HiHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Say hi = new Say("Hi");
+        Say hello = new Say("Hello");
+        hi.start();
+        hello.start();
+    }
+    
+}
